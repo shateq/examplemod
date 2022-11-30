e3ca0c7eba5e3b22f2f5868a785188e718329bfa
Szymon <shateq21@gmail.com>
2022-11-30 14:40:20 +0100

    Gradle Wrapper  7.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,12 @@ description = "This is an example description! Tell everyone what your mod is ab
 
 dependencies {
     minecraft("com.mojang:minecraft:${project.extra["mc"]}")
-    modImplementation("net.fabricmc:fabric-loader:${project.extra["loader"]}")
     mappings("net.fabricmc:yarn:${project.extra["yarn"]}:v2")
-    //Alternatively mojang maps
-    //mappings(loom.officialMojangMappings())
+//    mappings(loom.officialMojangMappings()) //mojang mappings
     //Alternatively ParchmentMC maps
     //mappings(loom.layered { officialMojangMappings(); parchment("org.parchmentmc.data:parchment-1.19.2:${project.extra["parchment"]}") })
 
-    //Fabric API
+    modImplementation("net.fabricmc:fabric-loader:${project.extra["loader"]}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.extra["fapi"]}")
     //Deprecated FabricAPI modules
     //modImplementation "net.fabricmc.fabric-api:fabric-api-deprecated:${project.fabric_version}"
@@ -60,18 +58,19 @@ tasks {
 }
 
 modrinth {
-    token.set(System.getenv("MODRINTH_TOKEN")) //Remember to have the MODRINTH_TOKEN environment variable set or else this will fail, or set it to whatever you want - just make sure it stays private!
-    projectId.set("my-project") //This can be the project ID or the slug. Either will work!
-    versionName.set("${project.version} for ${project.extra["mc"]}")
+    //Remember to have the MODRINTH_TOKEN environment variable set or else this will fail, or set it to whatever you want - just make sure it stays private!
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("my-project") //This can be the project ID or the slug.
+
+    versionName.set("${project.version} for MC ${project.extra["mc"]}")
     versionNumber.set(version.toString())
     versionType.set("alpha") // 'release', 'alpha', 'beta'
 
     uploadFile.set(tasks[tasks.remapJar.name]) // With Loom, this MUST be set to `remapJar` instead of `jar`!
-    gameVersions.addAll("1.19", "1.19.1", "1.19.3") // Must be an array, even with only one version
-    loaders.addAll("fabric") // Must also be an array - no need to specify this if you're using Loom or ForgeGradle
+    gameVersions.addAll("1.19", "1.19.1", "1.19.2")
     dependencies {
         // scope.type: can be `required`, `optional`, `incompatible`, or `embedded`
         // The type can either be `project` or `version`
-        required.project("fabric-api") // Creates a new required dependency on Fabric API
+        required.project("fabric-api")
     }
 }
